64bbb394ab4c3b647a524a0054f3b549e1a59501
thinme <thinme12@naver.com>
2022-06-08 16:54:27 +0900

    6-8commit

--- log script.js
+++ log script.js
@@ -11,3 +11,12 @@ id=prompt('아이디 입력');
 else{
     location.href="20_error.html"
 }
+function solution(string) {
+    var answer = '';
+    let check=0;
+    for(let i=0; i<string.length; i++){
+        check=(string[i]==="")?0:check+1;
+        answer+=(check%2 ===0)?string[i].toLowerCase():string[i].toUpperCase();
+    }
+    return answer
+}
