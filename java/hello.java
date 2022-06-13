756645844a4520fa5537ac52ccb763d2adbd4285
thinme <thinme12@naver.com>
2022-06-13 14:09:54 +0900

    commit

---
+++ java/hello.java
@@ -0,0 +1,7 @@
+//package studyJava13;
+
+class hello{
+    public static void main(String[] arg){
+        System.out.println("output");
+    }
+}
---
+++ java/test.java
@@ -0,0 +1,9 @@
+//pacage 작성
+
+//파일명과 클래스명 동일
+class Main{
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
+
